6617857a100afeba89857949f3593a1aeca78131
renachiouo <magirenaouo@gmail.com>
2025-12-09 14:06:00 +0800

    FILTER UPDATE

--- api/enrich-attribution.js
+++ api/enrich-attribution.js
@@ -99,6 +99,19 @@ async function fetchTwitch(endpoint, params = {}) {
 }
 
 export default async function handler(request, response) {
+    // Enable CORS
+    response.setHeader('Access-Control-Allow-Credentials', true);
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    response.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+    );
+
+    if (request.method === 'OPTIONS') {
+        return response.status(200).end();
+    }
+
     if (request.method !== 'POST') {
         return response.status(405).json({ error: 'Method Not Allowed' });
     }
