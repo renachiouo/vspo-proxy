8be0d20c78933dc50cc8cde146bbe7ea3398e51c
renachiouo <magirenaouo@gmail.com>
2026-04-28 12:29:03 +0800

    Outbound_Bandwidth_Reduction_FIX

--- api/youtube.js
+++ api/youtube.js
@@ -2090,7 +2090,7 @@ export default async function handler(req, res) {
         }
 
         // Cache-Control for videos list
-        res.setHeader('Cache-Control', 's-maxage=900, stale-while-revalidate=900');
+        res.setHeader('Cache-Control', 's-maxage=900, stale-while-revalidate=300');
 
         const lang = searchParams.get('lang') || 'cn';
         const isForeign = lang === 'jp';
